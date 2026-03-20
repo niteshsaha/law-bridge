0f61076067ba3aef364e6aba6e90ea0461931735
nitesh <nitesh.rokz@gmail.com>
2026-03-20 18:27:49 +0530

    Update API_BASE URL for deployment

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 #API_BASE = "http://127.0.0.1:8000"
-API_BASE = "https://law-bridge-api.onrender.com" #for render deployment
+API_BASE = "https://law-bridge-1-gc9l.onrender.com" #for render deployment
 
 # ==============================
 # 🔹 CONFIG
